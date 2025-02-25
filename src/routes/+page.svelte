3e9f7f8eabd66e5df4bcb0ebcc1bccb293311234
Alexander Hildebrandt <alex@hilde.dev>
2025-02-25 20:58:09 +0100

    new base-app style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to Chorehub</h1>
+<p>To login or register, <a href="/demo/lucia/login">click here</a></p>
---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton-svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="h-screen grid grid-rows-[auto_1fr_auto]">
+	<!-- Header -->
+	<header class="preset-filled-primary-800-200 p-4">
+		<Avatar name="A H" background="preset-filled-secondary-500" classes="w-10 h-10 rounded-full text-lg flex" />
+	</header>
+	<!-- Main -->
+	<main class="preset-filled-surface-100-900 p-4 space-y-4">
+		{@render children()}
+	</main>
+	<!-- Footer -->
+	<footer class="preset-filled-primary-200-800 p-4">(footer)</footer>
+</div>
