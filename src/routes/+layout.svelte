87f84807c8c6d05d98194d9dad620c63b1cfb44f
Alexander Hildebrandt <alex@hilde.dev>
2025-02-26 20:33:52 +0100

    move header to separate file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
-	import { Avatar } from '@skeletonlabs/skeleton-svelte';
+	import AppHeader from './AppHeader.svelte';
 
 	let { children, data } = $props();
 </script>
@@ -10,12 +10,7 @@
 <ParaglideJS {i18n}>
 	<div class="h-screen grid grid-rows-[auto_1fr_auto]">
 		<!-- Header -->
-		<header class="preset-filled-primary-800-200 p-4 flex items-center justify-between">
-			<div class="flex items-center gap-1">
-				<Avatar name={data.user?.username ?? ''} background="preset-filled-secondary-500" classes="w-10 h-10 rounded-full text-lg flex" />
-			</div>
-			<a href="/users" class="btn preset-filled-primary-100-900">Switch User</a>
-		</header>
+		<AppHeader user={data.user}></AppHeader>
 		<!-- Main -->
 		<main class="preset-filled-surface-100-900 p-4 space-y-4">
 			{@render children()}
---
+++ src/routes/AppHeader.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton-svelte';
+
+	let { user } = $props();
+</script>
+<header class="preset-filled-primary-800-200 p-4 flex items-center justify-between">
+	<div class="flex items-center gap-1">
+		<Avatar name={user?.username ?? ''} background="preset-filled-secondary-500"
+						classes="w-10 h-10 rounded-full text-lg flex" />
+	</div>
+	<a href="/users" class="btn preset-filled-primary-100-900">Switch User</a>
+</header>
