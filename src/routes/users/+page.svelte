347255f2dc9ebdffc3b6a62bbb1429341049ea80
Alexander Hildebrandt <alex@hilde.dev>
2025-02-26 20:53:09 +0100

    improve styling for switch-user

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -6,13 +6,15 @@
 {#await data.users}
 	loading...
 {:then users}
-	{#each users as user}
-		<form method="POST" action="?/select">
-			<input type="hidden" name="userId" value={user.id}>
-			<button type="submit" class="card preset-filled-secondary-100-900">{user.username}</button>
-		</form>
-	{/each}
-	<a class="card preset-filled-primary-100-900" href="/users/add">
-		Add new user
-	</a>
+	<div class="flex gap-4 justify-center items-center w-full h-full">
+		{#each users as user}
+			<form method="POST" action="?/select">
+				<input type="hidden" name="userId" value={user.id}>
+				<button type="submit" class="card btn preset-filled-secondary-100-900 w-32 h-24">{user.username}</button>
+			</form>
+		{/each}
+		<a class="card btn preset-filled-primary-100-900 w-32 h-24" href="/users/add">
+			Add new user
+		</a>
+	</div>
 {/await}
