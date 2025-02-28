b5a7256348b37343b9645d631d87b8270dde76e7
Alexander Hildebrandt <alex@hilde.dev>
2025-02-28 11:46:03 +0100

    improve category visuals

--- src/routes/shopping/+page.svelte
+++ src/routes/shopping/+page.svelte
@@ -7,7 +7,13 @@
 {:then categories}
 	<div class="flex flex-col gap-4 justify-center items-center w-full h-full">
 		{#each categories as category}
-			<a href="/shopping/{category.id}" class="card btn preset-filled-secondary-100-900 w-full h-36">{category.name}</a>
+			<div class="card preset-filled-secondary-100-900 w-full h-36">
+				<!-- Header -->
+				<div class="flex justify-between p-2">
+					{category.name}
+					<a href="/shopping/{category.id}" class="btn border">Edit</a>
+				</div>
+			</div>
 		{/each}
 		<a class="card btn preset-filled-primary-100-900 w-36 h-24" href="/shopping/add">
 			Add new category
