e8dba952f99bf5363e9a78502599776b46cf4dc7
Alexander Hildebrandt <alex@hilde.dev>
2025-02-28 11:55:47 +0100

    move category card into separate component

--- src/routes/shopping/+page.svelte
+++ src/routes/shopping/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import CategoryCard from './CategoryCard.svelte';
+
 	let { data } = $props();
 </script>
 
@@ -7,13 +9,7 @@
 {:then categories}
 	<div class="flex flex-col gap-4 justify-center items-center w-full h-full">
 		{#each categories as category}
-			<div class="card preset-filled-secondary-100-900 w-full h-36">
-				<!-- Header -->
-				<div class="flex justify-between p-2">
-					{category.name}
-					<a href="/shopping/{category.id}" class="btn border">Edit</a>
-				</div>
-			</div>
+			<CategoryCard {category}></CategoryCard>
 		{/each}
 		<a class="card btn preset-filled-primary-100-900 w-36 h-24" href="/shopping/add">
 			Add new category
---
+++ src/routes/shopping/CategoryCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+	let { category } = $props();
+</script>
+
+<div class="card preset-filled-secondary-100-900 w-full h-36">
+	<!-- Header -->
+	<div class="flex justify-between p-2">
+		{category.name}
+		<a href="/shopping/{category.id}" class="btn border">Edit</a>
+	</div>
+</div>
