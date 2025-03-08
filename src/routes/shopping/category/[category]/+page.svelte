eccb8320a3e302a1576e1cfebeba36454fdb93b1
Alexander Hildebrandt <alex@hilde.dev>
2025-03-08 21:25:30 +0100

    improve category edit page styling

--- src/routes/shopping/category/[category]/+page.svelte
+++ src/routes/shopping/category/[category]/+page.svelte
@@ -7,16 +7,23 @@
   let categoryName = $state(data.category?.name || '');
 </script>
 
-<h1>{data.category ? 'Edit Category' : 'Add New Category'}</h1>
-
-<form method="POST" use:enhance>
-	<input
-		type="text"
-		name="name"
-		placeholder="Category name"
-		bind:value={categoryName}
-		required
-	/>
-	<input type="hidden" name="categoryId" value={data.category?.id}>
-	<button type="submit">{data.category ? 'Update' : 'Create'}</button>
-</form>
+<div class="flex justify-center items-center h-full bg-surface-100">
+	<div class="card p-8  preset-filled-primary-100-900">
+		<div class="h4 mb-2">{data.category ? 'Edit Category' : 'Add New Category'}</div>
+		<form method="POST" use:enhance>
+			<input type="hidden" name="categoryId" value={data.category?.id}>
+			<div class="mb-4">
+				<label>
+					Name
+					<input class="input preset-filled-primary-50-950 border" type="text" name="name" bind:value={categoryName}
+								 required />
+				</label>
+			</div>
+			<div class="flex flex-col sm:flex-row gap-2">
+				<button type="submit" class="btn preset-filled-primary-800-200 variant-outline w-full sm:w-1/2">
+					{data.category ? 'Update' : 'Create'}
+				</button>
+			</div>
+		</form>
+	</div>
+</div>
