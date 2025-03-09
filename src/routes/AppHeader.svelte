00acc6312c1cd3dd16d74ae48cf5b9e1b0a197d2
Alexander Hildebrandt <alex@hilde.dev>
2025-03-09 09:51:59 +0100

    improve header links

--- src/routes/AppHeader.svelte
+++ src/routes/AppHeader.svelte
@@ -3,10 +3,14 @@
 
   let { user } = $props();
 </script>
-<header class="preset-filled-primary-800-200 p-4 flex items-center justify-between">
+<header class="preset-filled-primary-100-900 p-4 flex items-center justify-between">
+	<a href="/" class="btn">Home</a>
 	<div class="flex items-center gap-1">
-		<Avatar name={user?.username ?? ''} background="preset-filled-secondary-500"
-						classes="w-10 h-10 rounded-full text-lg flex" />
+		<a href="/users">
+			<Avatar name={user?.username ?? ''}
+							background="preset-filled-secondary-500"
+							classes="w-10 h-10 rounded-full text-lg flex"
+			/>
+		</a>
 	</div>
-	<a href="/users" class="btn preset-filled-primary-100-900">Switch User</a>
 </header>
