01f948aaa84b2848e016d0714c28d163af56e805
Alexander Hildebrandt <alex@hilde.dev>
2025-03-09 14:13:03 +0100

    add active flag to items, don't delete them anymore

--- src/lib/server/db/functions.ts
+++ src/lib/server/db/functions.ts
@@ -1,88 +1,106 @@
 import * as table from '$lib/server/db/schema';
 import { shoppingCategory, type ShoppingCategory, shoppingItem, type User } from '$lib/server/db/schema';
 import { db } from '$lib/server/db/index';
-import { asc, eq, inArray, max } from 'drizzle-orm';
+import { and, asc, eq, inArray, max } from 'drizzle-orm';
 import { encodeBase32LowerCase } from '@oslojs/encoding';
 
 // ------- USER RELATED -------
 export const findUser = async (userId: string): Promise<User | undefined> => {
-	const result = await db.select().from(table.user).where(eq(table.user.id, userId));
+  const result = await db.select().from(table.user).where(eq(table.user.id, userId));
 
-	return result.at(0);
+  return result.at(0);
 };
 
 export const findAllUsers = async (): Promise<User[]> => {
-	return db.select().from(table.user).execute();
+  return db.select().from(table.user).execute();
 };
 
 export const addUser = async (username: string): Promise<void> => {
-	await db.insert(table.user).values({ id: generateUUID(), username }).execute();
+  await db.insert(table.user).values({ id: generateUUID(), username }).execute();
 };
 
 // ------- SHOPPING RELATED -------
 export const findShoppingCategory = async (categoryId: string): Promise<ShoppingCategory | undefined> => {
-	const result = await db
-		.select()
-		.from(table.shoppingCategory)
-		.where(eq(table.shoppingCategory.id, categoryId));
+  const result = await db
+    .select()
+    .from(table.shoppingCategory)
+    .where(eq(table.shoppingCategory.id, categoryId));
 
-	return result.at(0);
+  return result.at(0);
 };
 
 export const findAllShoppingCategories = async (): Promise<ShoppingCategory[]> => {
-	return db.query.shoppingCategory.findMany({
-		with: {
-			shoppingItems: {
-				orderBy: [asc(shoppingItem.priority)]
-			}
-		},
-		orderBy: [asc(shoppingCategory.priority)]
-	}).execute();
+  return db.query.shoppingCategory.findMany({
+    with: {
+      shoppingItems: {
+        orderBy: [asc(shoppingItem.priority)],
+        where: eq(shoppingItem.active, true)
+      }
+    },
+    orderBy: [asc(shoppingCategory.priority)]
+  }).execute();
 };
 
 export const updateShoppingCategory = async (categoryId: string, name: string): Promise<void> => {
-	await db.update(table.shoppingCategory).set({ name: name }).where(eq(table.shoppingCategory.id, categoryId)).execute();
+  await db.update(table.shoppingCategory).set({ name: name }).where(eq(table.shoppingCategory.id, categoryId)).execute();
 };
 
 export const addShoppingCategory = async (name: string): Promise<void> => {
-	const currentMaxPriority = (
-		await db
-			.select({ value: max(table.shoppingCategory.priority) })
-			.from(table.shoppingCategory)
-	).at(0);
-	const nextPriority = typeof currentMaxPriority?.value === 'number' ? currentMaxPriority.value + 1 : 0;
+  const currentMaxPriority = (
+    await db
+      .select({ value: max(table.shoppingCategory.priority) })
+      .from(table.shoppingCategory)
+  ).at(0);
+  const nextPriority = typeof currentMaxPriority?.value === 'number' ? currentMaxPriority.value + 1 : 0;
 
-	await db.insert(table.shoppingCategory).values({
-		id: generateUUID(),
-		name: name,
-		priority: nextPriority
-	});
+  await db.insert(table.shoppingCategory).values({
+    id: generateUUID(),
+    name: name,
+    priority: nextPriority
+  });
 };
 
 export const addShoppingItem = async (categoryId: string, name: string): Promise<void> => {
-	const currentMaxPriority = (
-		await db
-			.select({ value: max(table.shoppingItem.priority) })
-			.from(table.shoppingItem)
-			.where(eq(table.shoppingItem.categoryId, categoryId))
-	).at(0);
-	const nextPriority = typeof currentMaxPriority?.value === 'number' ? currentMaxPriority.value + 1 : 0;
+  // If we have a matching inactive item, we just make it active again.
+  const existingItemId = (await db.select({ id: table.shoppingItem.id })
+      .from(table.shoppingItem)
+      .where(and(
+        eq(table.shoppingItem.categoryId, categoryId),
+        eq(table.shoppingItem.name, name),
+        eq(table.shoppingItem.active, false)
+      ))
+  ).at(0)?.id;
+  if (existingItemId) {
+    await db.update(table.shoppingItem).set({ active: true }).where(eq(table.shoppingItem.id, existingItemId)).execute();
 
-	await db.insert(table.shoppingItem).values({
-		id: generateUUID(),
-		categoryId: categoryId,
-		name: name,
-		priority: nextPriority
-	});
+    return;
+  }
+
+  // Otherwise we add a new item at the end of the list.
+  const currentMaxPriority = (
+    await db
+      .select({ value: max(table.shoppingItem.priority) })
+      .from(table.shoppingItem)
+      .where(eq(table.shoppingItem.categoryId, categoryId))
+  ).at(0);
+  const nextPriority = typeof currentMaxPriority?.value === 'number' ? currentMaxPriority.value + 1 : 0;
+
+  await db.insert(table.shoppingItem).values({
+    id: generateUUID(),
+    categoryId: categoryId,
+    name: name,
+    priority: nextPriority,
+    active: true
+  });
 };
 
 export const removeShoppingItems = async (itemIds: string[]): Promise<void> => {
-	await db.delete(table.shoppingItem).where(inArray(table.shoppingItem.id, itemIds)).execute()
+  await db.update(table.shoppingItem).set({ active: false }).where(inArray(table.shoppingItem.id, itemIds)).execute();
 };
 
 // ------- GENERIC -------
 function generateUUID() {
-	// ID with 120 bits of entropy, or about the same as UUID v4.
-	const bytes = crypto.getRandomValues(new Uint8Array(15));
-	return encodeBase32LowerCase(bytes);
+  // ID with 120 bits of entropy, or about the same as UUID v4.
+  const bytes = crypto.getRandomValues(new Uint8Array(15));
+  return encodeBase32LowerCase(bytes);
 }
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,36 +1,37 @@
-import { integer, pgTable, text, unique } from 'drizzle-orm/pg-core';
+import { boolean, integer, pgTable, text, unique } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
 
 export const user = pgTable('user', {
-    id: text().primaryKey(),
-    username: text().notNull().unique(),
+  id: text().primaryKey(),
+  username: text().notNull().unique()
 });
 export type User = typeof user.$inferSelect;
 
 export const shoppingCategory = pgTable('shopping_category', {
-    id: text().primaryKey(),
-    name: text().notNull().unique(),
-    priority: integer().notNull().unique(),
+  id: text().primaryKey(),
+  name: text().notNull().unique(),
+  priority: integer().notNull().unique()
 });
 export type ShoppingCategory = typeof shoppingCategory.$inferSelect;
 
 export const shoppingCategoryRelations = relations(shoppingCategory, ({ many }) => ({
-	shoppingItems: many(shoppingItem),
+  shoppingItems: many(shoppingItem)
 }));
 
 export const shoppingItem = pgTable('shopping_item', {
-    id: text().primaryKey(),
-    categoryId: text().references(() => shoppingCategory.id),
-    name: text().notNull().unique(),
-    priority: integer().notNull(),
+  id: text().primaryKey(),
+  categoryId: text().references(() => shoppingCategory.id),
+  name: text().notNull().unique(),
+  priority: integer().notNull(),
+  active: boolean().notNull(),
 }, (shoppingItem) => [
-	unique().on(shoppingItem.categoryId, shoppingItem.priority),
+  unique().on(shoppingItem.categoryId, shoppingItem.priority)
 ]);
 export type ShoppingItem = typeof shoppingItem.$inferSelect;
 
 export const shoppingItemRelations = relations(shoppingItem, ({ one }) => ({
-	category: one(shoppingCategory, {
-		fields: [shoppingItem.categoryId],
-		references: [shoppingCategory.id],
-	}),
+  category: one(shoppingCategory, {
+    fields: [shoppingItem.categoryId],
+    references: [shoppingCategory.id]
+  })
 }));
