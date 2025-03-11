79fa383a66d87706419cc1dec424bdfe9080f578
Alexander Hildebrandt <alex@hilde.dev>
2025-03-11 15:42:39 +0100

    allow purchase to finish without adding items

--- src/routes/shopping/purchase/+page.server.ts
+++ src/routes/shopping/purchase/+page.server.ts
@@ -6,11 +6,13 @@ export const actions = {
     const data = await request.formData();
     let items = data.getAll('items').map((formValue) => formValue.toString());
     let user = locals.user;
-    if (!user || items.length === 0) {
+    if (!user) {
       return fail(400, { message: 'Missing data' });
     }
 
-    await createPurchase(user, items);
+    if (items.length > 0) {
+      await createPurchase(user, items);
+    }
 
     return redirect(302, '/shopping');
   }
