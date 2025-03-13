06e917022e811b95cff4b372251559523ac57508
Alexander Hildebrandt <alex@hilde.dev>
2025-03-13 10:17:56 +0100

    move to yarn-modern

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,30 @@ import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(), paraglide({
-        project: "./project.inlang",
-        outdir: "./src/lib/paraglide"
-    })]
+  plugins: [
+    sveltekit(),
+    tailwindcss(),
+    paraglide({
+      project: './project.inlang',
+      outdir: './src/lib/paraglide',
+    }),
+  ],
+  server: {
+    fs: {
+      allow: [
+        '.',
+        process.env.XDG_CACHE_HOME
+          ? path.join(process.env.XDG_CACHE_HOME, 'yarn/berry/cache/')
+          : path.join(process.env.HOME || '~', '.cache/yarn/berry/cache/'),
+        path.resolve(__dirname, './.yarn/.cache'),
+      ],
+    },
+  },
 });
