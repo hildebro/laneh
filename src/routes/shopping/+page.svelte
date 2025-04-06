c56cb2cd62a85c5e2923fcc507af423692da9c72
Alexander Hildebrandt <alex@hilde.dev>
2025-04-06 16:41:58 +0200

    put shopping categories into a grid

--- src/routes/shopping/+page.svelte
+++ src/routes/shopping/+page.svelte
@@ -4,16 +4,20 @@
   let { data } = $props();
 </script>
 
-{#await data.categories}
-	loading...
-{:then categories}
-	<div class="flex flex-wrap gap-2 justify-center">
-		<a class="btn" href="shopping/item/add">Add items</a>
-		<a class="btn" href="shopping/category/add">New category</a>
-		<a class="btn" href="shopping/category/reorder">Reorder categories</a>
-		<a class="btn" href="shopping/purchase" >Start purchase</a>
-	</div>
-	{#each categories as category}
-		<CategoryCard {category}></CategoryCard>
-	{/each}
-{/await}
+<div class="card w-full">
+  {#await data.categories}
+    loading...
+  {:then categories}
+    <div class="flex justify-end gap-2 mb-4">
+      <a class="btn" href="shopping/item/add">Add items</a>
+      <a class="btn" href="shopping/category/add">New category</a>
+      <a class="btn" href="shopping/category/reorder">Reorder categories</a>
+      <a class="btn" href="shopping/purchase">Start purchase</a>
+    </div>
+    <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-2 mb-4">
+      {#each categories as category}
+        <CategoryCard {category}></CategoryCard>
+      {/each}
+    </div>
+  {/await}
+</div>
--- src/routes/shopping/CategoryCard.svelte
+++ src/routes/shopping/CategoryCard.svelte
@@ -4,16 +4,16 @@
   let { category } = $props();
 </script>
 
-<div class="card w-full">
-	<!-- Header -->
-	<div class="flex justify-between">
-		<b>{category.name}</b>
-		<a href="shopping/category/{category.id}" class="btn">Edit</a>
-	</div>
-	<!-- Item list -->
-	<div class="mb-2">
-		{#each category.shoppingItems.filter((item: ShoppingItem) => item.active) as item}
-			- {item.amount} {item.name}<br />
-		{/each}
-	</div>
+<div class="card preset-filled-secondary-100-900">
+  <!-- Header -->
+  <div class="flex justify-between">
+    <b>{category.name}</b>
+    <a href="shopping/category/{category.id}" class="btn">Edit</a>
+  </div>
+  <!-- Item list -->
+  <div class="mb-2">
+    {#each category.shoppingItems.filter((item: ShoppingItem) => item.active) as item}
+      - {item.amount} {item.name}<br />
+    {/each}
+  </div>
 </div>
