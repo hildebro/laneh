f93ca672e08078e02f55d4354a93371f2febb69b
Alexander Hildebrandt <alex@hilde.dev>
2025-04-10 10:26:55 +0200

    enforce eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,30 +6,47 @@ import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
   ...ts.configs.recommended,
-  ...svelte.configs["flat/recommended"],
+  ...svelte.configs['flat/recommended'],
   prettier,
   ...svelte.configs['flat/prettier'],
   {
     languageOptions: {
-	  globals: {
-	    ...globals.browser,
-	    ...globals.node
-	  }
-	}
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    plugins: {
+      import: await import('eslint-plugin-import').then(m => m.default)
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index']
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          }
+        }
+      ]
+    }
   },
   {
-    files: ["**/*.svelte"],
-
+    files: ['**/*.svelte'],
     languageOptions: {
-	  parserOptions: {
-	    parser: ts.parser
-	  }
-	}
+      parserOptions: {
+        parser: ts.parser
+      }
+    }
   }
 );
