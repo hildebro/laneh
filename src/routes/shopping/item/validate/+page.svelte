244fdca4cdd401f8dcfd55ec5dcfac66eecb27ed
Alexander Hildebrandt <alex@hilde.dev>
2025-04-18 16:05:57 +0200

    translate validate page
    expect for table headers that will be removed soon

--- src/routes/shopping/item/validate/+page.svelte
+++ src/routes/shopping/item/validate/+page.svelte
@@ -1,40 +1,21 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
+  import * as m from '$lib/paraglide/messages.js';
 
   let { data } = $props();
 
   let closeMatchItems = $state(data.items.filter(item => item.status === 'close_match'));
-  let perfectMatchItems = $state(data.items.filter(item => item.status === 'perfect_match'));
-  let unmatchedItems = $state(data.items.filter(item => item.status === 'unmatched'));
 
   let isSubmitting = $state(false);
 </script>
 
 <svelte:head>
-  <title>Validate Shopping Items</title>
+  <title>{ m.shopping_validate_headline() }</title>
 </svelte:head>
 
 <div class="card">
-  <h1 class="text-2xl font-semibold mb-4">Validate Shopping Items</h1>
+  <h1 class="text-2xl font-semibold mb-4">{ m.shopping_validate_headline() }</h1>
 
-  <b>Existing items</b> (will be reactivated and receive the new amount you defined):
-  <ul>
-    {#each perfectMatchItems as item(item.id)}
-      <li>
-        - {item.amount} {item.name}
-      </li>
-    {/each}
-  </ul>
-  <br />
-  <b>New items</b> (must be categorized in the next step):
-  <ul>
-    {#each unmatchedItems as item(item.id)}
-      <li>
-        - {item.amount} {item.name}
-      </li>
-    {/each}
-  </ul>
-  <br />
   <form
     method="POST"
     use:enhance={() => {
@@ -45,10 +26,7 @@
         };
       }}
   >
-    <b>Potentially existing items</b> (look very similar to existing items).
-    <br />
-    By default, your input items will be renamed to match the existing items. You can prevent the
-    renaming to create new items instead by clicking the associated checkbox.
+    { m.shopping_validate_explanation() }
     <div class="table-container preset-filled-primary-200-800 mt-2">
       <table class="table text-base">
         <thead>
@@ -72,12 +50,19 @@
 
     <div class="mt-6 text-center">
       <button type="submit" class="btn" disabled={isSubmitting}>
-        {#if isSubmitting}
-          <span class="loading loading-spinner loading-xs"></span> Saving...
-        {:else}
-          Continue
-        {/if}
+        { m.generic_continue() }
       </button>
     </div>
   </form>
+
+  <br />
+
+  { m.shopping_validate_all_items() }
+  <ul>
+    {#each data.items as item(item.id)}
+      <li>
+        - {item.amount} {item.name}
+      </li>
+    {/each}
+  </ul>
 </div>
