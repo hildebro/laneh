636b1369ba7b5a9e87701595946882c3b72ee6bf
Alexander Hildebrandt <alex@hilde.dev>
2025-04-27 19:07:39 +0200

    add language switcher

--- src/routes/settings/users/+page.svelte
+++ src/routes/settings/users/+page.svelte
@@ -1,16 +1,40 @@
 <script lang="ts">
   import { Avatar } from '@skeletonlabs/skeleton-svelte';
   import { enhance } from '$app/forms';
+  import { goto } from '$app/navigation'; // Import available tags and the current tag function
+  import { base } from '$app/paths';
   import LoadingSpinner from '$lib/LoadingSpinner.svelte';
   import * as m from '$lib/paraglide/messages.js';
+  import { availableLanguageTags, sourceLanguageTag } from '$lib/paraglide/runtime.js';
 
   let { data } = $props();
+
+  function changeLanguage(targetLang: string) {
+    if (targetLang === sourceLanguageTag) {
+      goto(`${base}/settings/users`);
+    } else {
+      goto(`${base}/${targetLang}/settings/users`);
+    }
+  }
 </script>
 
-<div class="card">
-  {#await data.users}
-    <LoadingSpinner/>
-  {:then users}
+{#await data.users}
+  <div class="card">
+    <LoadingSpinner />
+  </div>
+{:then users}
+  <div class="card mb-2 w-full">
+    <h1 class="text-2xl font-semibold mb-4">{ m.settings_users_language() }</h1>
+    <div class="flex flex-wrap justify-center gap-4">
+      {#each availableLanguageTags as lang(lang)}
+        <button class="btn" onclick={() => changeLanguage(lang)}>
+          {lang}
+        </button>
+      {/each}
+    </div>
+  </div>
+  <div class="card w-full">
+    <h1 class="text-2xl font-semibold mb-4">{ m.settings_users_switch() }</h1>
     <div class="flex flex-wrap justify-center gap-4">
       {#each users as user (user.id)}
         <form method="POST" action="?/select" use:enhance>
@@ -28,5 +52,5 @@
         {m.settings_users_add()}
       </a>
     </div>
-  {/await}
-</div>
+  </div>
+{/await}
