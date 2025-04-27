0bf32400b06bc6f46fdd44dc7a2d8ac783322d2a
Alexander Hildebrandt <alex@hilde.dev>
2025-04-27 16:20:17 +0200

    add footer with username

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   import { ParaglideJS } from '@inlang/paraglide-sveltekit';
   import AppHeader from './AppHeader.svelte';
   import { i18n } from '$lib/i18n';
+  import * as m from '$lib/paraglide/messages.js';
 
   let { children, data } = $props();
 </script>
@@ -13,9 +14,14 @@
 
 <ParaglideJS {i18n}>
   <div class="h-screen grid grid-rows-[auto_1fr_auto] overflow-y-auto">
-    <AppHeader user={data.user}></AppHeader>
+    <AppHeader />
     <main class="flex flex-col items-center p-4">
       {@render children()}
     </main>
+    <footer class="text-right text-xs p-1">
+      {#if data.user}
+        { m.footer_user({ name: data.user.username }) }
+      {/if}
+    </footer>
   </div>
 </ParaglideJS>
