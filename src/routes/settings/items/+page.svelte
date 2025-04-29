aececbf2bc00f200e82ada828443471f659dd1a1
Alexander Hildebrandt <alex@hilde.dev>
2025-04-29 20:58:09 +0200

    implement confirm-modal for deletion

--- src/routes/settings/items/+page.svelte
+++ src/routes/settings/items/+page.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
+  import { Modal } from '@skeletonlabs/skeleton-svelte';
   import { enhance } from '$app/forms';
   import CategorizedItemSelect from '$lib/CategorizedItemSelect.svelte';
   import LoadingSpinner from '$lib/LoadingSpinner.svelte';
   import * as m from '$lib/paraglide/messages.js';
+
+  let openState = $state(false);
+
+  // Reference to the form DOM element
+  let formElement: HTMLFormElement | undefined = $state();
+
+  function modalClose() {
+    openState = false;
+  }
+
+  /**
+   * Function to handle the form submit. Can't rely on submit button, because the modal that contains that button
+   * is rendered outside of the form in the DOM.
+   */
+  function handleConfirmSubmit() {
+    // Use requestSubmit() which is often preferred as it respects validation and behaves more like a user
+    // click on a submit button. submit() would also work but might bypass some checks.
+    formElement?.requestSubmit();
+    modalClose();
+  }
 
   let { data, form } = $props();
 </script>
@@ -10,11 +31,36 @@
 {#await data.categories}
   <LoadingSpinner />
 {:then categories}
-  <form class="flex flex-col gap-4 items-center h-full w-full" method="POST" use:enhance>
+  <form
+    class="flex flex-col gap-4 items-center h-full w-full"
+    method="POST"
+    use:enhance
+    bind:this={formElement}
+  >
     <CategorizedItemSelect {categories} unfiltered />
     {#if form?.message}
       <p class="card preset-filled-error-50-950 rounded text-center">{form.message}</p>
     {/if}
-    <button class="btn ml-auto" type="submit">{m.settings_items_delete()}</button>
+    <Modal
+      open={openState}
+      onOpenChange={(e) => (openState = e.open)}
+      triggerBase="btn preset-filled-error-800-200"
+      contentBase="card preset-filled-warning-100-900 p-4 space-y-4 shadow-xl max-w-screen-sm"
+      backdropClasses="backdrop-blur-sm"
+    >
+      {#snippet trigger()}{m.settings_items_delete()}{/snippet}
+      {#snippet content()}
+        <h2 class="h2">{m.settings_items_delete()}</h2>
+        <p class="opacity-60">{ m.settings_items_delete_info() }</p>
+        <div class="flex justify-end gap-4">
+          <button type="button" class="btn preset-filled-warning-800-200" onclick={modalClose}>
+            { m.generic_cancel() }
+          </button>
+          <button type="button" class="btn preset-filled-warning-800-200" onclick={handleConfirmSubmit}>
+            { m.generic_confirm() }
+          </button>
+        </div>
+      {/snippet}
+    </Modal>
   </form>
 {/await}
