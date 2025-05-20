1a186363dfec476228abef85391012017ffa0c71
Alexander Hildebrandt <alex@hilde.dev>
2025-05-20 22:24:08 +0200

    translate and improve task edit

--- src/routes/schedule/[task]/+page.svelte
+++ src/routes/schedule/[task]/+page.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
   import * as m from '$lib/paraglide/messages.js';
+  import type { Weekday } from '$lib/server/db/schema';
 
   let { data, form } = $props();
 
   let name = $state(data.task?.name);
   let weekday = $state(data.task?.dueWeekday);
   let userId = $state(data.task?.nextDueUserId);
+
+  function translateWeekday(weekday: Weekday) {
+    switch (weekday) {
+      case 'mon':
+        return m.schedule_weekday_mon()
+      case 'tue':
+        return m.schedule_weekday_tue()
+      case 'wed':
+        return m.schedule_weekday_wed()
+      case 'thu':
+        return m.schedule_weekday_thu()
+      case 'fri':
+        return m.schedule_weekday_fri()
+      case 'sat':
+        return m.schedule_weekday_sat()
+      case 'sun':
+        return m.schedule_weekday_sun()
+    }
+  }
 </script>
 
 <div class="card">
@@ -21,23 +41,23 @@
     <input type="hidden" name="taskId" value={data.task?.id}>
     <label>
       { m.generic_name() }
-      <input class="form-input input" type="text" name="name" bind:value={name} />
+      <input class="form-input input mb-2" type="text" name="name" bind:value={name} />
     </label>
 
     <label>
-      Weekday
-      <select class="select" name="weekday" bind:value={weekday}>
-        <option value="" disabled selected>--Please choose a weekday--</option>
+      { m.schedule_weekday() }
+      <select class="select mb-2" name="weekday" bind:value={weekday}>
+        <option value="" disabled selected>{ m.generic_required() }</option>
         {#each data.weekdays as weekdayOption (weekdayOption)}
-          <option value={weekdayOption}>{weekdayOption}</option>
+          <option value={weekdayOption}>{translateWeekday(weekdayOption)}</option>
         {/each}
       </select>
     </label>
 
     <label>
-      User
-      <select class="select" name="userId" bind:value={userId}>
-        <option value="" disabled selected>--Please choose a user--</option>
+      { m.schedule_next_assignee() }
+      <select class="select mb-2" name="userId" bind:value={userId}>
+        <option value="" disabled selected>{ m.generic_required() }</option>
         {#await data.users then users}
           {#each users as user (user.id)}
             <option value={user.id}>{user.username}</option>
