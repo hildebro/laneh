dde69b5224179be7947dd66b7744e3274361df82
Alexander Hildebrandt <alex@hilde.dev>
2025-05-28 17:13:02 +0200

    improve auth redirects

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,8 +3,16 @@ import type { LayoutServerLoad } from './$types';
 import { base } from '$app/paths';
 
 export const load: LayoutServerLoad = async (event) => {
+  // Redirect to auth, if not authenticated and not already at that path.
   if (!event.locals.authenticated && !event.route.id?.startsWith('/auth')) {
-    return redirect(302, `${base}/auth`);
+    let redirectTarget = `${base}/auth`;
+    // Add the original target to the auth route via query param to redirect after authentication.
+    if (event.route.id && event.route.id !== '/') {
+      // Substring(1) to remove the leading slash from the target.
+      redirectTarget += `?target=${encodeURI(event.route.id.substring(1))}`;
+    }
+
+    return redirect(302, redirectTarget);
   }
 
   // Make sure to check for authentication first. No need to check for users before authenticating.
--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,9 +1,16 @@
 import { type Actions, fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 import { base } from '$app/paths';
 import * as m from '$lib/paraglide/messages.js';
 import { createSession, verifyOTP } from '$lib/server/auth';
 import { processForm } from '$lib/server/formHandler';
 import { z } from '$lib/zod';
+
+export const load: PageServerLoad = async ({ locals }) => {
+  if (locals.authenticated) {
+    return redirect(302, base);
+  }
+};
 
 const otpSchema = z.object({
   code: z.string().trim().nonempty()
@@ -11,12 +18,9 @@ const otpSchema = z.object({
 
 export const actions: Actions = {
   default: async (event) => {
-    return processForm(event, otpSchema, async (otp, { cookies }) => {
-      if (await verifyOTP(otp.code)) {
-        await createSession(cookies);
-
-        return redirect(302, `${base}`);
-      } else {
+    return processForm(event, otpSchema, async (otp, { cookies, url }) => {
+      const verified = await verifyOTP(otp.code);
+      if (!verified) {
         // Fill the error date with zod-like data, so it's handled the same way by the frontend.
         return fail(422, {
           issues: [
@@ -24,6 +28,14 @@ export const actions: Actions = {
           ]
         });
       }
+
+      await createSession(cookies);
+
+      if (url.searchParams.has('target')) {
+        return redirect(302, `${base}/${url.searchParams.get('target')}`);
+      }
+
+      return redirect(302, `${base}`);
     });
   }
 };
