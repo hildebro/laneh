01560e7b2941f7a3ce1000930380eac793a6a354
Alexander Hildebrandt <alex@hilde.dev>
2025-05-28 17:48:47 +0200

    use enhancedform for purchases

--- src/routes/shopping/purchase/+page.server.ts
+++ src/routes/shopping/purchase/+page.server.ts
@@ -1,17 +1,29 @@
 import { redirect } from '@sveltejs/kit';
 import { createPurchase } from '$lib/server/db/functions';
 import type { User } from '$lib/server/db/schema';
+import { processForm } from '$lib/server/formHandler';
+import { z } from '$lib/zod';
 
-export const actions = {
-  default: async ({ request, locals }) => {
-    const data = await request.formData();
-    const items = data.getAll('items').map((formValue) => formValue.toString());
-    const user = locals.user as User;
-
-    if (items.length > 0) {
-      await createPurchase(user, items);
+const purchaseSchema = z.object({
+  itemIds: z.transform((val: string) => {
+    if (!val || val === '') {
+      return [];
     }
 
-    return redirect(302, '../shopping');
+    return val.split(',');
+  }).pipe(z.array(z.string().nonempty()))
+});
+
+export const actions = {
+  default: async (event) => {
+    return processForm(event, purchaseSchema, async (purchase, { locals }) => {
+      const user = locals.user as User;
+
+      if (purchase.itemIds.length > 0) {
+        await createPurchase(user, purchase.itemIds);
+      }
+
+      return redirect(302, '../shopping');
+    });
   }
 };
