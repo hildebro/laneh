6237184ebd877444ebc6a024a3417359f5a63e94
Alexander Hildebrandt <alex@hilde.dev>
2025-11-05 23:34:21 +0100

    fix navigation

--- src/routes/AppHeader.svelte
+++ src/routes/AppHeader.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   // noinspection ES6UnusedImports Intellij doesn't resolve the dot annotation correctly
   import { Navigation } from '@skeletonlabs/skeleton-svelte';
-  import { CalendarDays, House, Settings, ShoppingCart, Star } from 'lucide-svelte';
+  import { CalendarDays, House, Settings, ShoppingCart } from 'lucide-svelte';
   import { base } from '$app/paths';
   import { page } from '$app/state';
   import * as m from '$lib/paraglide/messages.js';
@@ -40,32 +40,37 @@
       value = newActiveId; // Update the $state variable
     }
   });
+
+  let anchorBar = 'btn preset-filled-surface-100-900 hover:preset-tonal flex-col items-center gap-1';
 </script>
 
-<header class="sticky top-0 z-50 bg-surface-50-950/75 backdrop-blur-xs">
-  <Navigation.Bar {value} background="">
-    <Navigation.Tile id="dashboard" href={base} label={m.header_dashboard()}>
-      <House />
-    </Navigation.Tile>
-    <Navigation.Tile id="shopping" href="{base}/shopping" label={m.header_shopping()}>
-      <ShoppingCart />
-    </Navigation.Tile>
-    <Navigation.Tile id="schedule" href="{base}/schedule" label={m.header_schedule()}>
-      <div class="relative">
-        <CalendarDays />
-        {#if dueTaskCount > 0}
+<header class="sticky top-0 z-50 bg-surface-50-950 backdrop-blur-xs">
+  <Navigation layout="bar">
+    <Navigation.Menu class="grid grid-cols-4 gap-2">
+      <a href={base} class={anchorBar}>
+        <House />
+        <span>{m.header_dashboard()}</span>
+      </a>
+      <a href="{base}/shopping" class={anchorBar}>
+        <ShoppingCart />
+        <span>{m.header_shopping()}</span>
+      </a>
+      <a href="{base}/schedule" class={anchorBar}>
+        <div class="relative">
+          <CalendarDays />
+          {#if dueTaskCount > 0}
           <span
             class="absolute -top-3.5 -right-3.5 px-2 py-1 text-xs font-bold leading-none preset-filled-error-500 rounded-full">
             {dueTaskCount}
           </span>
-        {/if}
-      </div>
-    </Navigation.Tile>
-    <Navigation.Tile label={m.header_daily_upkeep()} labelClasses="disabled">
-      <Star />
-    </Navigation.Tile>
-    <Navigation.Tile id="settings" href="{base}/settings" label={m.header_settings()}>
-      <Settings />
-    </Navigation.Tile>
-  </Navigation.Bar>
+          {/if}
+        </div>
+        <span>{m.header_schedule()}</span>
+      </a>
+      <a href="{base}/settings" class={anchorBar}>
+        <Settings />
+        <span>{m.header_settings()}</span>
+      </a>
+    </Navigation.Menu>
+  </Navigation>
 </header>
