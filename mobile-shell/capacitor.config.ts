5ade2f55f77c656a1f08cf2beaa63923df15b909
Alexander Hildebrandt <alex@hilde.dev>
2026-02-25 21:12:04 +0100

    ms: add hot-reloading possibility

--- mobile-shell/capacitor.config.ts
+++ mobile-shell/capacitor.config.ts
@@ -1,21 +1,24 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
+const devServerUrl = process.env.CAP_SERVER_URL;
+
 const config: CapacitorConfig = {
-	appId: 'com.chorehub.app',
-	appName: 'Chorehub',
-	webDir: 'build',
-	plugins: {
-		CapacitorHttp: {
-			enabled: true // Intercepts fetch() and routes it natively
-		}
-	},
-	server: {
-		allowNavigation: ['*'],
-		cleartext: true
-	},
-	android: {
-		allowMixedContent: true
-	}
+  appId: 'com.chorehub.app',
+  appName: 'Chorehub',
+  webDir: 'build',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true // Intercepts fetch() and routes it natively
+    }
+  },
+  server: {
+    allowNavigation: ['*'],
+    cleartext: true,
+    ...(devServerUrl && { url: devServerUrl })
+  },
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
