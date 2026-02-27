9c0e74c8fbec1b3181f7e2e90216976440717a2b
Alexander Hildebrandt <alex@hilde.dev>
2026-02-27 13:38:52 +0100

    reintroduce schedule badge

--- src/routes/AppHeader.svelte
+++ src/routes/AppHeader.svelte
@@ -6,21 +6,35 @@
 
   let { dueTaskCount } = $props();
 
-  const links = [
+  const links = $derived([
     { label: m.header_dashboard(), href: resolve('/'), icon: House },
     { label: m.header_shopping(), href: resolve('/shopping'), icon: ShoppingCart },
     { label: m.header_balance(), href: resolve('/balance'), icon: Receipt },
-    { label: m.header_schedule(), href: resolve('/schedule'), icon: CalendarDays },
+    {
+      label: m.header_schedule(),
+      href: resolve('/schedule'),
+      icon: CalendarDays,
+      badge: dueTaskCount > 0 ? dueTaskCount : null
+    },
     { label: m.header_settings(), href: resolve('/settings'), icon: Settings }
-  ];
+  ]);
 </script>
 
 <Navigation layout="bar">
   <Navigation.Menu class="grid grid-cols-5 gap-1">
     {#each links as link (link)}
       {@const Icon = link.icon}
-      <Navigation.TriggerAnchor href={link.href}>
-        <Icon class="size-8" />
+      <Navigation.TriggerAnchor href={link.href} class="flex flex-col items-center">
+
+        <div class="relative inline-block">
+          {#if link.badge}
+            <span
+              class="badge-icon preset-filled-error-500 absolute -top-2.5 -right-4 z-10">
+              {link.badge}
+            </span>
+          {/if}
+          <Icon class="size-8" />
+        </div>
         <Navigation.TriggerText>{link.label}</Navigation.TriggerText>
       </Navigation.TriggerAnchor>
     {/each}
