ab65c5feb52fc1c3cb27c2784b912013bda55a0b
Alexander Hildebrandt <alex@hilde.dev>
2026-04-20 17:03:37 +0200

    fix: hard-coded server url for capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'dev.laneh.app',
   appName: 'Laneh',
   webDir: 'build',
-  server: {
-    url: 'http://192.168.178.75:5173',
-    cleartext: true // Required since you are using HTTP, not HTTPS
-  }
 };
 
 export default config;
