ed8e70f7e0f551102df0ab0a6836993ed0418917
Alexander Hildebrandt <alex@hilde.dev>
2026-04-20 20:26:23 +0200

    fix: node and static build, based on env

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// Check if we are building for mobile (Capacitor)
+const isCapacitor = process.env.CAPACITOR_BUILD === 'true';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
-    }),
+    adapter: isCapacitor
+      ? adapterStatic({
+        pages: 'build',
+        assets: 'build',
+        fallback: 'index.html',
+        precompress: false,
+        strict: true
+      })
+      : adapterNode()
   }
 };
 
