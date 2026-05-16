c257be2d402eba0f39ca734ddb552f2faf7e8d9e
Alexander Hildebrandt <alex@hilde.dev>
2026-05-16 23:19:09 +0200

    feat: partial migrate /tasks/single/add

--- src/routes/(authenticated)/tasks/single/[task]/+page.svelte
+++ src/routes/(authenticated)/tasks/single/[task]/+page.svelte
@@ -3,6 +3,7 @@
   import { getApiClient } from '$lib/apiClient';
   import ApiForm from '$lib/components/ApiForm.svelte';
   import * as m from '$lib/paraglide/messages.js';
+  import ApiFormItem from '$lib/components/ApiFormItem.svelte';
 
   let { data } = $props();
 
@@ -23,23 +24,21 @@
   <h2>{ id ? m.schedule_single_task_edit() : m.schedule_single_task_add() }</h2>
 
   <ApiForm submitAction={saveTask} onSuccess={resolve('/tasks')}>
-    <label>
-      { m.generic_name() }
-      <input type="text" bind:value={name} required />
-    </label>
-
-    <label>
-      { m.schedule_single_task_assignee() }
-      <select bind:value={dueUserId}>
-        <option value=""></option>
-        {#await data.users then users}
-          {#each users as user (user.id)}
-            <option value={user.id}>{user.username}</option>
-          {/each}
-        {/await}
-      </select>
-    </label>
-
+    <ApiFormItem
+      label={m.generic_name()}
+      name="name"
+      bind:value={name}
+    />
+    <ApiFormItem
+      label={m.schedule_single_task_assignee()}
+      name="assignee"
+      type="select"
+      bind:value={dueUserId}
+    >
+      {#each data.users as user (user.id)}
+        <option value={user.id}>{user.username}</option>
+      {/each}
+    </ApiFormItem>
     <label>
       { m.schedule_single_task_due_date() }
       <input type="date" bind:value={dueDate} />
